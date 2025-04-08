12d4dd46610ad42c0d7769e6ecb44a84e7a883db
Leo-march <leonardomarcheti2@gmail.com>
2025-04-08 09:22:34 -0300

    Se mata github

--- Aula 3/index.js
+++ Aula 3/index.js
@@ -6,24 +6,56 @@ app.use(express.json()); //Vou habilitar json no express
 
 //rota para criar usuário
 
-app.post('/users', (req,res) =>{
-    const {nome, email} = req.body;
-    if(!nome || !email){
-        return res.status(400).json
-        ({error: "Nome e Email são dados obrigatórios"})
-    }
+app.post('/users', async (req, res) => {
+    try {
+        const { nome, email, senha, endereco, telefone, cpf } = req.body;
+        if (!nome || !email || !senha || !endereco || !telefone || !cpf) {
+            return res.status(400).json
+                ({ error: "Nome, email, senha, endereço, telefone e CPF são dados obrigatórios" })
+        }
+        const user = await userService.addUser(nome, email, senha, endereco, telefone, cpf);
+        res.status(200).json({ user });
+    } catch (erro) {
+        console.log("Erro ao adicionar o usuário", erro)
+        res.status(401).json({ error: erro.message});
 
-    const user = userService.addUser(nome, email);
-    res.status(200).json({user});
+    }
 })
 
 //rota para mostrar os usuários
 
-app.get("/users", (req, res) =>{
-    res.json(userService.getUsers)
+app.get("/users", (req, res) => {
+    res.json(userService.getUsers())
+});
+
+app.delete("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);//Converte o ID para número
+    try {
+        const resultado = userService.deleteUser(id);//Tenta deletar o usuário
+        res.status(200).json(resultado);//Se conseguir deletar, retorna 200
+    }
+    catch {
+        console.log("Erro ao deletar o usuário", erro)
+    }
+    userService.deleteUser(id);
+    res.status(200).json({ message: "Usuário deletado com sucesso" })
+})
+
+app.put("/users/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome, email, senha, endereco, telefone, cpf } = req.body;
+    try {
+        const resultado = await userService.updateUser(id, nome, email, senha, endereco, telefone, cpf);
+        if (!resultado) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
+        res.status(200).json(resultado);
+    } catch (erro) {
+        res.status(401).json({ error: erro.message});
+    }
 });
 
 const port = 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Servidor rodando na porta", port);
 })
