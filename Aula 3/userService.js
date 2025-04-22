e57ad4a4ee38babcaff0cef190ebbaac5e588b49
Leo-march <leonardomarcheti2@gmail.com>
2025-04-22 08:39:32 -0300

    limpada de código(retirado Load, save, getNextId e tirou as coisas do construtor)

--- Aula 3/userService.js
+++ Aula 3/userService.js
@@ -6,41 +6,6 @@ const { error } = require('console');
 const mysql = require('./mysql')
 
 class userService {
-    constructor() {
-        this.filePath = path.join(__dirname, 'user.json');
-        this.users = this.loadUsers();//Array para armazenar user
-        this.nextid = this.getNextId();//icontador para gerar id
-    }
-
-    loadUsers() {
-        try {
-            if (fs.existsSync(this.filePath)) {//verifica se o arquivo existe
-                const data = fs.readFileSync(this.filePath);//le o arquivo
-                return JSON.parse(data);//transforma json em objeto
-            }
-        } catch (erro) {
-            console.log("Erro ao carregar o arquivo", erro)
-        }
-        return []
-    }
-
-
-    getNextId() { // função para buscar o próximo id
-        try {
-            if (this.users.length === 0) return 1;
-            return Math.max(...this.users.map(user => user.id)) + 1
-        } catch (erro) {
-            console.log('Erro ao buscar o id', erro)
-        }
-    }
-
-    saveUsers() {//função para salvar os arquivos
-        try {
-            fs.writeFileSync(this.filePath, JSON.stringify(this.users));
-        } catch (erro) {
-            console.log("Erro ao salvar arquivos", erro)
-        }
-    }
 
     async addUser(nome, email, senha, endereco, telefone, cpf) {//função para adicionar um usuário
         try {
